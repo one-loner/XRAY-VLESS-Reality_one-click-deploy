97adc293c0f712dfebea4f8aa02648bf685367a6
one-loner <one-loner>
2024-08-22 17:24:39 +0300

    Add client config

--- install.sh
+++ install.sh
@@ -3,14 +3,20 @@ if (($EUID !=0)); then
      echo Script must be run by root.
      exit
 fi
-
+echo "Updating and Upgrading software."
+apt-get update
+apt-get upgrade -y
 echo "Installing requirements. "
 apt-get install -y docker docker-compose curl qrencode
 ip=$(curl http://ifconfig.me)
 uuuid=$(cat /proc/sys/kernel/random/uuid)
 cp config.json config.json.original
+cp client_config.json client_config.json.original
 sed -i "s/uuuuid/$uuuid/g" config.json
-echo 'File config.json is changed by your settings. Original files saved with .original postfix.'
+sed -i "s/uuuuid/$uuuid/g" client_config.json
+sed -i "s/ipaddr/$ip/g" client_config.json
+
+echo 'Files config.json and client_config.json is changed by your settings. Original files saved with .original postfix.'
 sleep 5
 docker-compose up -d
 docker ps
@@ -25,3 +31,4 @@ echo ''
 echo ''
 echo $link > links.txt
 echo 'Link saved in file links.txt'
+echo 'Client configuration in json format you can find in client_config.json'
